3bd20d2d1b42b9bfb7b18305e273c24cbe1e91b7
Almir <161244867+AlmiirCosta@users.noreply.github.com>
2025-01-06 15:55:49 -0300

    feat: crai as rotas do controller com mock

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -0,0 +1,30 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
+
+public class MissoesController {
+
+    @GetMapping("/listar")
+    public String listarMissoes(){
+        return "Missões listadas";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Ninja criado";
+    }
+
+    @PutMapping("/alterarmissao")
+    public String alterarMissao(){
+        return "Ninja alterado";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarNinja(){
+        return "Ninja deletado";
+    }
+
+}
